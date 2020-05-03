1bc73d8bf7fab5d5025a056f5b1d014efa856736
Matt B <malanb5@gmail.com>
2020-05-03 12:54:08 -0700

    separated predict and run function. run is now the main function to execute

--- YJ/WalRunner.py
+++ YJ/WalRunner.py
@@ -6,7 +6,7 @@ import pandas as pd, numpy as np,math
 from tqdm import tqdm
 import pickle
 import concurrent.futures
-import environment as env
+from YJ.environment import *
 import os, argparse, logging
 import Shaper as Shaper
 from YJ.FManager import load, save
@@ -166,7 +166,7 @@ class WalRunner:
 			yhat = WalRunner._prophet_predict(i, df, fut_dat, Prophet)
 			s_pred.append(yhat)
 
-		pickle.dump(s_pred, open(env.WORKING_DIR + '/eda_objs/s_predt_prophet_%d_%d.pkl'%(start_i, fin_i), 'wb'))
+		pickle.dump(s_pred, open(WORKING_DIR + '/eda_objs/s_predt_prophet_%d_%d.pkl'%(start_i, fin_i), 'wb'))
 
 		return i, s_pred
 
@@ -176,8 +176,8 @@ class WalRunner:
 
 		eda_obs_fp = '/objs/eda_objs/'
 
-		pob = pickle.load(open(env.WORKING_DIR + eda_obs_fp + 'timeseries_sales.pkl', "rb"))
-		fut_dat = pickle.load(open(env.WORKING_DIR + eda_obs_fp + 'predict_cal', 'rb'))
+		pob = pickle.load(open(WORKING_DIR + eda_obs_fp + 'timeseries_sales.pkl', "rb"))
+		fut_dat = pickle.load(open(WORKING_DIR + eda_obs_fp + 'predict_cal', 'rb'))
 
 		if not mt:
 			s_pred=list()
@@ -188,9 +188,9 @@ class WalRunner:
 				yhat = WalRunner._prophet_predict(i, df, fut_dat, Prophet)
 				s_pred.append(yhat)
 				if i % 100 == 0:
-					pickle.dump(s_pred, open(env.WORKING_DIR + eda_obs_fp + 's_predt_prophet_holidays_4_20_20_13500_30000.pkl', 'wb'))
+					pickle.dump(s_pred, open(WORKING_DIR + eda_obs_fp + 's_predt_prophet_holidays_4_20_20_13500_30000.pkl', 'wb'))
 
-			pickle.dump(s_pred, open(env.WORKING_DIR + eda_obs_fp +'s_predt_prophet_holidays_4_20_20_13500_30000.pkl', 'wb'))
+			pickle.dump(s_pred, open(WORKING_DIR + eda_obs_fp +'s_predt_prophet_holidays_4_20_20_13500_30000.pkl', 'wb'))
 
 		elif(mt):
 			import time
@@ -209,7 +209,7 @@ class WalRunner:
 					id, res = f.result()
 					pred_sales[id] = res
 
-			pickle.dump(pred_sales, open(env.WORKING_DIR + '/eda_objs/prophet_pred_sales_mt_18294_24392.pkl', "wb"))
+			pickle.dump(pred_sales, open(WORKING_DIR + '/eda_objs/prophet_pred_sales_mt_18294_24392.pkl', "wb"))
 
 			print("--- %s seconds ---" % (time.time() - start_time))
 			exit(0)
@@ -411,7 +411,7 @@ class WalRunner:
 				ds[featName] = getattr(ds["date"].dt, featFunc).astype("int16")
 
 	@staticmethod
-	def lgbm_get_model(lg, X_train, y_train,  params, catFeats, model_fp, isTrained= False):
+	def lgbm_get_model(lg, X_train, y_train, isTrained= False):
 		import lightgbm as lgb
 		import gc
 
@@ -421,27 +421,25 @@ class WalRunner:
 			trainInds = np.setdiff1d(X_train.index.values, validInds)
 
 			trainData = lgb.Dataset(X_train.loc[trainInds], label=y_train.loc[trainInds],
-									categorical_feature=catFeats, free_raw_data=False)
+									categorical_feature=CAT_FEATURES, free_raw_data=False)
 			validData = lgb.Dataset(X_train.loc[validInds], label=y_train.loc[validInds],
-									categorical_feature=catFeats, free_raw_data=False)
+									categorical_feature=CAT_FEATURES, free_raw_data=False)
 
 			lg.debug("cleaning up necessarily data structures...")
 			del X_train, y_train, validInds, trainInds
 			gc.collect()
 
-
-
 			lg.debug("training lgb model...")
 
 			# Train LightGBM model
-			m_lgb = lgb.train(params, trainData, valid_sets=[validData], verbose_eval=20)
+			m_lgb = lgb.train(LGBM_PARAMS, trainData, valid_sets=[validData], verbose_eval=20)
 
 			# # Save the model
-			m_lgb.save_model(model_fp)
+			m_lgb.save_model(MODEL_FP)
 
 		else:
 			# loading the pre-trained model
-			m_lgb = lgb.Booster(model_file=model_fp)
+			m_lgb = lgb.Booster(model_file=MODEL_FP)
 
 		return m_lgb
 
@@ -456,83 +454,55 @@ class WalRunner:
 
 
 	@staticmethod
-	def lgbm_predict(lg):
-
-		np.random.seed(777)
-
-		# LOADING
-		objs_fps = ["objs/X_train_LGBmodel.pkl", "objs/y_train_LGBmodel.pkl", "objs/trainCols_LGBmodel.pkl",
-					"objs/calendarCols_LGBmodel.pkl", "objs/priceCols_LGBmodel.pkl"]
-
-		catFeats = ['item_id', 'dept_id', 'store_id', 'cat_id', 'state_id'] + \
-				   ["event_name_1", "event_name_2", "event_type_1", "event_type_2"]
-
-		model_fp = "models/model_events_before.lgb"
-		# model parameters
-		params = {
-			"objective": "poisson",
-			"metric": "rmse",
-			"force_row_wise": True,
-			"learning_rate": 0.075,
-			"sub_row": 0.75,
-			"bagging_freq": 1,
-			"lambda_l2": 0.1,
-			"metric": ["rmse"],
-			'verbosity': 1,
-			'num_iterations': 1200,
-			'num_leaves': 128,
-			"min_data_in_leaf": 100,
-		}
-
-		X_train, y_train, trainCols, calendar, prices  = WalRunner.load_objs(objs_fps)
-
-		m_lgb = WalRunner.lgbm_get_model(lg, X_train, y_train, model_fp =model_fp, params=params, catFeats=catFeats, isTrained=True)
-
+	def lgbm_predict(lg, prices, calendar, trainCols, m_lgb):
 		# PREDICTIONS
 		lg.debug("making predictions...")
 
-		# Last day used for training
-		trLast = 1913
-		# Maximum lag day
-		maxLags = 57
-
-
-		fday = datetime(2016, 4, 25)
-		alphas = [1.028, 1.023, 1.018]
-		weights = [1 / len(alphas)] * len(alphas)
-		sub = 0.
-
-		for icount, (alpha, weight) in enumerate(zip(alphas, weights)):
-			te = WalRunner.create_ds(trLast, maxLags, calendar, prices)
+		for icount, (alpha, weight) in enumerate(zip(ALPHAS, WEIGHTS)):
+			te = WalRunner.create_ds(TR_LAST, MAX_LAGS, calendar, prices)
 			cols = [f"F{i}" for i in range(1, 29)]
 
 			for tdelta in range(0, 28):
-				day = fday + timedelta(days=tdelta)
-				lg.debug("%s, %s"%(tdelta, day))
-				tst = te[(te['date'] >= day - timedelta(days=maxLags)) & (te['date'] <= day)].copy()
+				day = FDAY + timedelta(days=tdelta)
+				lg.debug("%s, %s" % (tdelta, day))
+				tst = te[(te['date'] >= day - timedelta(days=MAX_LAGS)) & (te['date'] <= day)].copy()
 				WalRunner.create_features(tst)
 				tst = tst.loc[tst['date'] == day, trainCols]
 				te.loc[te['date'] == day, "sales"] = alpha * m_lgb.predict(tst)  # magic multiplier by kyakovlev
 
-			te_sub = te.loc[te['date'] >= fday, ["id", "sales"]].copy()
+			te_sub = te.loc[te['date'] >= FDAY, ["id", "sales"]].copy()
 			te_sub["F"] = [f"F{rank}" for rank in te_sub.groupby("id")["id"].cumcount() + 1]
 			te_sub = te_sub.set_index(["id", "F"]).unstack()["sales"][cols].reset_index()
 			te_sub.fillna(0., inplace=True)
 			te_sub.sort_values("id", inplace=True)
 			te_sub.reset_index(drop=True, inplace=True)
 			te_sub.to_csv(f"submission_{icount}.csv", index=False)
+
 			if icount == 0:
 				sub = te_sub
 				sub[cols] *= weight
 			else:
 				sub[cols] += te_sub[cols] * weight
+
 			lg.debug(icount, alpha, weight)
 
 		lg.debug("creating csv file submissions")
 		sub2 = sub.copy()
-		sub2["id"] = sub2["id"].str.replace("validation$", "evaluation")
+		sub2["id"] = sub2["id"].str.replace("validation", "evaluation")
 		sub = pd.concat([sub, sub2], axis=0, sort=False)
-		sub.to_csv("submissions/submission_lgbm_tree_20_4_20.csv", index=False)
+		sub.to_csv(SUBMISSION_FP, index=False)
+
+	@staticmethod
+	def lgbm_run(lg):
+
+		np.random.seed(777)
+
+		X_train, y_train, trainCols, calendar, prices  = WalRunner.load_objs(DS_FPS)
+
+		m_lgb = WalRunner.lgbm_get_model(lg, X_train, y_train, isTrained=True)
+
+		WalRunner.lgbm_predict(lg, prices, calendar, trainCols, m_lgb)
+
 
 class Main:
 	@staticmethod
@@ -573,7 +543,7 @@ class Main:
 		if args.algorithm == "prophet":
 			WalRunner.prophet_predict(mt=False, lg=lg)
 		elif args.algorithm == "lgbm":
-			WalRunner.lgbm_predict(lg=lg)
+			WalRunner.lgbm_run(lg=lg)
 		else:
 			raise Exception("please specify an algorithm to run eg. --algorithm lgbm")
 
